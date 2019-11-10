b2ce7d886f35092b7efa3d95b433bca81606977e
xpectro93 <jonathanandrade@pursuit.org>
2019-11-10 17:23:26 -0500

    Add two problems

---
+++ fundamentals/unit-1-review/jon.js
@@ -0,0 +1,21 @@
+// Create a function that takes in an array of strings.
+// and returns an array where the last index of each string is uppercased
+
+// create the function with a while loop, then a second time with array method.
+let thirdUpArr = ['lowercase', 'uppercase', 'index', 'method', 'function'];
+
+const thirdUp = arr => {
+
+}
+// Expected output => ['lowercasE', 'uppercasE', 'indeX', 'methoD', 'functioN'];
+
+
+//Create a function that takes in an array of numbers, and return only the numbers that are even after 1 is added to their value.
+// Solve using a while loop, then using a for loop
+//
+let isEvenPlusOne = [2,5,7,8,1,2,57,99]
+const isEvenPlusOne = arr => {
+
+}
+// Expected output => [5,7,1,57,99]
+
