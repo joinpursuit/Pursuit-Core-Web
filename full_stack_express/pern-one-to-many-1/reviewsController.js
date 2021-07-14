0f5c07329a8a6b3f9ee365325a12128ca8bfbe3f
Tristan Angieri <15096174+a-n-g-i-e-r-i@users.noreply.github.com>
2021-07-14 13:07:02 -0400

    Corrected erroneous code and increased consistency

--- full_stack_express/pern-one-to-many-1/reviewsController.js
+++ full_stack_express/pern-one-to-many-1/reviewsController.js
@@ -1,59 +1,69 @@
+// DEPENDENCIES
 const express = require("express");
 const reviews = express.Router({ mergeParams: true });
 const {
   getAllReviews,
   getReview,
   newReview,
-  deleteReview,
   updateReview,
+  deleteReview,
 } = require("../queries/reviews");
 
-// INDEX
+// ROUTES
+
+/* INDEX */
 reviews.get("/", async (req, res) => {
   try {
     const allReviews = await getAllReviews();
-    res.json(allReviews);
-  } catch (err) {
-    res.json(err);
+    console.log(`controller function call: ${allReviews}`);
+    res.status(200).json(allReviews);
+  } catch (e) {
+    res.status(404).statusMessage(e);
   }
 });
 
-// SHOW
+/* SHOW */
 reviews.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  const review = await getReview(id);
-  if (review.length > 0) {
-    res.json(review[0]);
-  } else {
-    res.status(404).json({ error: "not found" });
+  try {
+    const { id } = req.params;
+    const review = await getReview(id);
+    res.status(200).json(review);
+  } catch (e) {
+    res.status(404).statusMessage(e);
   }
 });
 
-// UPDATE
-reviews.put("/:id", async (req, res) => {
-  const { id } = req.params;
-  const updatedReview = await updateReview(id, req.body);
-  res.status(200).json(updatedReview);
-});
-
+/* CREATE */
 reviews.post("/", async (req, res) => {
-  const review = await newReview(req.body);
-  res.status(200).json(review);
+  try {
+    const review = await newReview(req.body);
+    res.status(200).json(review);
+  } catch (e) {
+    res.status(404).statusMessage(e);
+  }
 });
 
-// DELETE
+/* UPDATE */
+reviews.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedReview = await updateReview(id, req.body);
+    res.status(200).json(updatedReview);
+  } catch (e) {
+    res.status(404).statusMessage(e);
+  }
+});
+
+/* DELETE */
 reviews.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-  const deletedReview = await deleteReview(id);
-  res.status(200).json(deletedReview;
+  try {
+    const { id } = req.params;
+    const deletedReview = await deleteReview(id);
+    res.status(200).json(deletedReview);
+  } catch (e) {
+    res.status(404).statusMessage(e);
+  }
 });
 
-// TEST JSON NEW
-// {
-//     "reviewer":"Lou",
-//      "title": "Fryin Better",
-//      "content": "With the great tips and tricks I found here",
-//      "bookmark_id": "2",
-//      "rating": "4"
-// }
+// EXPORTS
 module.exports = reviews;
